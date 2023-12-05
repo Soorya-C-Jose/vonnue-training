9da04a141a8b0653769c6a3686f9feaeabf9dde1
Soorya-C-Jose <soorya.jose@vonnue.com>
2023-12-05 12:06:18 +0530

    js files are added

---
+++ Web-cloning/javascript/js pass2/Chrome Extension/script.js
@@ -0,0 +1,40 @@
+// let myLeads = []
+// const inputEl = document.getElementById('input-el')
+// const inputBtn = document.getElementById('input-btn');
+// const ulEl = document.getElementById('ul-el')
+
+
+// inputBtn.addEventListener("click", function () {
+//     myLeads.push(inputEl.value)
+//     inputEl.value = ''
+//     renderLeads()
+// })
+
+
+// function renderLeads() {
+//     let listItems = ""
+//     for (let i = 0; i < myLeads.length; i++) {
+//         listItems += `
+//     <li>
+//        <a target='_blank' href='${myLeads[i]}'>
+//            ${myLeads[i]}
+//          </a>
+//     </li>
+//     `
+//     }
+//     ulEl.innerHTML = listItems
+// }
+
+// const deleteBtn = document.getElementById("delete-btn")
+
+// deleteBtn.addEventListener("dblclick", function() {
+//     console.log("double clicked")
+// })
+
+
+const welcomeEl = document.getElementById("welcome-el")
+function greetUser(greeting) {
+    welcomeEl.textContent = greeting + ", per harald borgen"
+}
+
+greetUser('howdy')
---
+++ Web-cloning/javascript/js pass2/E-commerce/script.js
@@ -0,0 +1,6 @@
+let errorParagraph = document.getElementById('error')
+
+
+function purchase() {
+    errorParagraph.textContent = "Something went wrong, please try again"
+}
