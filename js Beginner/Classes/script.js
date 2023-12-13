8443a2f0afb99e72c31b2e7c787fc28f2dc94020
Soorya-C-Jose <soorya.jose@vonnue.com>
2023-12-13 13:56:46 +0530

    js files are added

--- js Beginner/Classes/script.js
+++ js Beginner/Classes/script.js
@@ -79,3 +79,192 @@ function Car(make,model,year) {
 const auto = new Car('Honda','Accord', '1998');
 console.log(auto instanceof Car);
 console.log(auto instanceof Object)
+
+//constructor property
+
+let F = function() {};
+let P = F.prototype;
+let C = P.constructor;
+console.log(C === F);
+
+
+/********CLASSES WITH THE CLASS KEYWORD*******/
+
+// class Range {
+//     constructor1(from, to) {
+//         this,from = from;
+//         this.to = to;
+//     }
+//     includes(x) { return this.from <= x && x <= this.to;}
+//     [Symbol.iterator]() {
+//         for(let x = Math.ceil(this.from); x <= this.to; x++) yield x;
+//     }
+//     toString() { return `(${this.from}...${this.to})`;}
+// }
+// let r2 = new Range(1,3)
+// console.log(r2.includes(2))
+// console.log(r.tostring)
+
+
+//Math.ceil
+
+console.log(Math.ceil(0.95));
+console.log(Math.ceil(4));
+console.log(Math.ceil(7.004))
+
+//static methods
+
+class classWithStaticMethod {
+    static staticProperty = 'someValue';
+    static staticMethod() {
+        return 'static method has been called.';
+    }
+    static {
+        console.log('class static initialization block called');
+    }
+}
+console.log(classWithStaticMethod.staticProperty);
+console.log(classWithStaticMethod.staticMethod());
+
+
+//getters and setters
+
+const student = {
+    firstName: 'Monica',
+    get getName() {
+        return this.firstName;
+    }
+};
+console.log(student.firstName)
+console.log(student.getName)
+// console.log(student.getName())
+
+
+const student1 = {
+    firstName: "Antony",
+    set changeName(newName) {
+        this.firstName = newName;
+    }
+};
+console.log(student1.firstName);
+student1.changeName = 'Sarah';
+console.log(student1.firstName);
+
+
+//complex number class
+
+class complex {
+    constructor(real, imaginary) {
+        this.r = real;
+        this.i = imaginary;
+    }
+    plus(that) {
+        return new complex(this.r + that.r, this.i + that.i);
+    }
+    times(that) {
+        return new complex(this.r * that.r - this.i * that.i, this.r * that.i + this.i * that.r);
+    }
+    static sum(c, d) {
+        return C.plus(d);
+    }
+    static product(c, d) {
+        return C.times(d);
+    }
+    get real() {
+        return this.r;
+    }
+    get imaginary() {
+        return this.i;
+    }
+    get magnitude() {
+        return Math.hypot(this.r, this.i);
+    }
+    toString() {
+        return `{${this.r},${this.i}}`;}
+      equals (that) {
+        return that instanceof complex && 
+        this.r === that.r && 
+        this.i === that.i;
+      }
+    }
+    complex.ZERO = new complex(0,0);
+    complex.ONE = new complex(1,0);
+    complex.I = new complex(0,1);
+ let c = new complex(2,3);
+let d = new complex(c.i, c.r);
+console.log(c.plus(d.toString()))
+console.log(c.magnitude)
+
+
+/******ADDING METHODS TO EXISTING CLASSES*****/
+
+//subclasses and prototypes
+
+
+//subclasses with extends and super
+
+class EZArray extends Array {
+    get first() {
+        return this[0];
+    }
+    get last() {
+        return this[this.length-1];
+    }
+}
+let a =  new EZArray();
+console.log(a instanceof EZArray)
+console.log(a instanceof Array)
+console.log(a.push(1,2,3,4))
+console.log(a.first)
+console.log(a.last)
+
+
+//the super keyword 
+
+class Foo {
+    constructor(name) {
+        this.name = name;
+    }
+    getNameSeparator() {
+        return '-';
+    }
+}
+class fooBar extends Foo {
+    constructor(name, index) {
+        super(name);
+        this.index = index;
+    }
+    getFullName() {
+        return this.name + super.getNameSeparator() + this.index;
+    }
+}
+const firstfooBar = new fooBar('foo',1);
+console.log(firstfooBar.name);
+console.log(firstfooBar.getFullName());
+
+
+//delegation instead of inheritence
+
+class histogram {
+    constructor() { this.map = new this.map(); }
+    count(key) {
+        return this.map.get(key) || 0;
+    }
+    has(key) { return this.count(key) > 0}
+    get size() { return this.map.size; }
+    add(key) {
+        this.map.set(key, this.count(key) + 1);
+    }
+    delete(key) {
+        let count = this.count(key);
+        if(count === 1) {
+            this.map.delete(key);
+        } else if( count > 1) {
+            this.map.set(key, count - 1);
+        }
+    }
+    [Symbol.iterator]() { return this.map.keys();}
+    keys() {return this.map.keys();}
+    values() {return this.map.values();}
+    entries() {return this.map.entries();}
+}
