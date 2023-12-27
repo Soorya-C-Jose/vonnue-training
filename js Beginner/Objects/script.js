f1ed9416dff80da6a3bf69441832b9b9f75f118d
Soorya-C-Jose <soorya.jose@vonnue.com>
2023-12-27 20:13:42 +0530

    files are added

--- js Beginner/Objects/script.js
+++ js Beginner/Objects/script.js
@@ -1,5 +1,5 @@
-const ages = {alice : 18, bob: 27};
-function hasPerson (name) {
+const ages = { alice: 18, bob: 27 };
+function hasPerson(name) {
     return name in ages;
 }
 function getAge(name) {
@@ -10,8 +10,8 @@ console.log(getAge("toString"));
 
 
 //assign()
-const target = { a:1, b:2};
-const source = { b:4, c:5};
+const target = { a: 1, b: 2 };
+const source = { b: 4, c: 5 };
 const returnedTarget = Object.assign(target, source);
 console.log(target)
 console.log(returnedTarget === target)
@@ -20,7 +20,7 @@ console.log(returnedTarget === target)
 //create()
 const person = {
     isHuman: false,
-    printIntroduction: function() {
+    printIntroduction: function () {
         console.log(`My name is ${this.name}. Am I human? ${this.isHuman}`);
     },
 };
@@ -29,13 +29,57 @@ me.name = 'Mathew';
 me.isHuman = true;
 console.log(me.printIntroduction())
 
-function fruits() {
-    this.name = 'fruit 1';
-}
-function apple() {
-    fruits.call(this);
+let student = {
+    name: "Lisa",
+    age: 24,
+    marks: 78.9,
+    display() {
+        console.log("Name:", this.name);
+    }
+};
+
+let std1 = Object.create(student);
+std1.name = "Sheeran";
+std1.display();
+
+
+//defineProperties()
+const object1 = {};
+Object.defineProperties(object1, {
+    property1: {
+        value: 42,
+        writable: true,
+    },
+    property2: {},
+});
+console.log(object1.property1)
+
+
+//entries()
+const object2 = {
+    a: 'somestring',
+    b: 42,
+};
+for (const [key, value] of Object.entries(object2)) {
+    console.log(`${key}: ${value}`);
 }
 
-apple.prototype = Object.create(fruits.prototype)
-const app = new apple();
-console.log(app.name)
+const obj = { foo: "bar", baz: 42 };
+console.log(Object.entries(obj));
+
+const arrayLike = { 0: "a", 1: "b", 2: "c" };
+console.log(Object.entries(arrayLike));
+const randomKeyOrder = { 100: "a", 2: "b", 7: "c" };
+console.log(Object.entries(randomKeyOrder));
+const myObj = Object.create(
+    {},
+    {
+        getFoo: {
+            value() {
+                return this.foo;
+            },
+        },
+    },
+);
+myObj.foo = "bar";
+console.log(Object.entries(myObj))
